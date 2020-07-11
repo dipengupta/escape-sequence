e3ad946f7e35ec0339fa68536e0587bf73ba8154
dipengupta <dipenrgupta@gmail.com>
2020-07-11 10:32:10 +0000

    migrated to mySQL

--- mysite7/settings.py
+++ mysite7/settings.py
@@ -74,13 +74,22 @@ WSGI_APPLICATION = 'mysite7.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'dipengupta$es',
+    'USER': 'dipengupta',
+    'PASSWORD': 'makeSiteFaster',
+    'HOST': 'dipengupta.mysql.pythonanywhere-services.com',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
