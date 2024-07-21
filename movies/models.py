e7bdb666aea84dcf8f5476d0fcc45da955fa60c2
dipeng <dipeng@urlinsgroup.com>
2024-07-21 18:11:13 -0400

    fixed names for database files upload

--- movies/models.py
+++ movies/models.py
@@ -1,14 +1,10 @@
 from django.db import models
-from django.conf import settings
 from django.db.models.signals import post_delete, pre_save
 from django.dispatch import receiver
-from uuid import uuid4
 import os
 
 
-
 '''
-
 The following follows a simple one-to-many DB scheme
 (i.e, one movie can have many reviews) 
 
@@ -18,13 +14,6 @@ Points to note:
  * def __str__(self): will return the string that will be seen in Django's Admin Form
  * class Meta: will be what the model shows up as in Django Admin
  * By default, deleting a Model's Object does not delete the uploaded files. Added code to take care of that.
-
-
-Things to implement:
-
-# Change the uploaded file's name to it's PK so that it does not clutter media folder
-
-
 '''
 
 
@@ -39,32 +28,15 @@ Things to implement:
 This serves the sole purpose to just have movies in-line to review. This is not related to any table.
 '''
 class UpcomingReviews(models.Model):
+    movieTitle = models.CharField(max_length=300)
+    releaseYear = models.IntegerField()
+    moviePoster = models.ImageField(upload_to='movies/upcoming', default='movies/posters/defaultPoster.jpg')
 
-	# def path_and_rename(instance, filename):
-	# 	upload_to = 'movies/upcoming'
-	# 	ext = filename.split('.')[-1]
-	# 	# get filename
-	# 	print('####')
-	# 	print(instance.pk)
-	# 	print(instance.id)
-	# 	print('####')
-	# 	if instance.pk:
-	# 		filename = '{}.{}'.format(instance.pk, ext)
-	# 	else:
-	# 		# set filename as random string
-	# 		filename = '{}.{}'.format(uuid4().hex, ext)
-	# 	# return the whole path to the file
-	# 	return os.path.join(upload_to, filename)
+    def __str__(self):
+        return self.movieTitle
 
-	movieTitle = models.CharField(max_length=300)
-	releaseYear = models.IntegerField()
-	moviePoster = models.ImageField(upload_to='movies/upcoming', default='movies/posters/defaultPoster.jpg')
-
-	def __str__(self):
-   		return self.movieTitle
-
-	class Meta:
-		verbose_name_plural = "Upcoming Reviews"
+    class Meta:
+        verbose_name_plural = "Upcoming Reviews"
 
 
 
@@ -75,134 +47,147 @@ class UpcomingReviews(models.Model):
 This is the main "table" where data for movies will be stored
 
 IMP: 
-	- here, "movieGenres" and "movieMoods" are essentially meant to act as tags that we can filter the movie out by later on.
-	- these can have multiple values, always put the new one on the next line, and use '-' instead of spaces
+    - here, "movieGenres" and "movieMoods" are essentially meant to act as tags that we can filter the movie out by later on.
+    - these can have multiple values, always put the new one on the next line, and use '-' instead of spaces
 
-	Eg:
+    Eg:
 
-	movieGenres:
-	[
-		comedy
-		coming-of-age
-	]
+    movieGenres:
+    [
+        comedy
+        coming-of-age
+    ]
 '''
+def upload_to(instance, filename):
+    ext = filename.split('.')[-1]  # Get file extension
+    filename = f"{instance.pk}_{instance.movieTitle}.{ext}"  # Create new filename
+    return os.path.join('movies/posters', filename)
+
 class AllMovies(models.Model):
-	movieTitle = models.CharField(max_length=300)
-	releaseYear = models.IntegerField()
-	moviePoster = models.ImageField(upload_to='movies/posters', default='movies/posters/defaultPoster.jpg')
-	movieSummary = models.TextField()
-	movieGenres = models.TextField()
-	movieMoods = models.TextField(blank=True)
-	movieAgeRating = models.TextField(blank=True)
-	movieDirectors = models.TextField(blank=True)
-	movieActors = models.TextField(blank=True)
+    movieTitle = models.CharField(max_length=300)
+    releaseYear = models.IntegerField()
+    moviePoster = models.ImageField(upload_to=upload_to, default='movies/posters/defaultPoster.jpg')
+# 	moviePoster = models.ImageField(upload_to='movies/posters', default='movies/posters/defaultPoster.jpg')
+    movieSummary = models.TextField()
+    movieGenres = models.TextField()
+    movieMoods = models.TextField(blank=True)
+    movieAgeRating = models.TextField(blank=True)
+    movieDirectors = models.TextField(blank=True)
+    movieActors = models.TextField(blank=True)
 
-	def __str__(self):
-   		return self.movieTitle
+    def save(self, *args, **kwargs):
+        # Save the instance first to ensure the primary key is set
+        if not self.pk:
+            super().save(*args, **kwargs)
+        # Update the file name with the primary key
+        self.moviePoster.name = upload_to(self, self.moviePoster.name)
+        # Save the instance again to apply the new file name
+        super().save(*args, **kwargs)
 
-	class Meta:
-		verbose_name_plural = "All Movies"
+    def __str__(self):
+        return self.movieTitle
 
-
-
-
-	'''
-
-	So, the idea behind this function is to get the data for who all gave it a "Thumbs Up".
-	This is quite a crude solution to a problem that should not exist in a good DB design, but hey, it works
-
-	Future improvement: right now, it will query for the same data over and over. We should have it run once and store it in the DB.
-
-
-	returns: dictionary with total likes and the int "1" which signifies that the person gave it a thumbs up
-	
-	IMP: the critera for comparison should be that "None" will be returned if the person gave it a thumbs down  
-	'''
-	def getOverallRating(self):
-
-		totalLikes = 0
-		like_ankur = None
-		like_dipen = None
-		like_ashesh = None
-		like_shantnu = None
-		like_jayant = None
-
-		primKey = self.id 
-
-
-		try:
-			movie_post = AllMovies.objects.get(id=primKey)
-		except:
-			movie_post = None
-
-
-		try:
-			ankur_post = Ankur.objects.get(post=primKey)
-			if ankur_post.verdict == 'thumbs_up':
-				totalLikes+=1
-				like_ankur = 1
-		except:
-			ankur_post = None
-
-
-		try:
-			dipen_post = Dipen.objects.get(post=primKey)
-			if dipen_post.verdict == 'thumbs_up':
-				totalLikes+=1
-				like_dipen = 1        
-		except:
-			dipen_post = None
-
-
-		try:
-			shantnu_post = Shantnu.objects.get(post=primKey)
-			if shantnu_post.verdict == 'thumbs_up':
-				totalLikes+=1
-				like_shantnu = 1
-		except:
-			shantnu_post = None
-
-
-		try:
-			ashesh_post = Ashesh.objects.get(post=primKey)
-			if ashesh_post.verdict == 'thumbs_up':
-				totalLikes+=1
-				like_ashesh = 1   
-		except:
-			ashesh_post = None
-
-
-		try:
-			jayant_post = Jayant.objects.get(post=primKey)
-			if jayant_post.verdict == 'thumbs_up':
-				totalLikes+=1
-				like_jayant = 1    
-		except:
-			jayant_post = None
-
-
-		finally:
-
-			verdictDict = {
-				'totalLikes': totalLikes,
-				'like_ankur' : like_ankur,
-				'like_dipen' : like_dipen,
-				'like_ashesh' : like_ashesh,
-				'like_shantnu' : like_shantnu,
-				'like_jayant' : like_jayant
-			}
-
-		return verdictDict
-
-
-	
-	def getMovieMoods(self):
-		return self.movieMoods
+    class Meta:
+        verbose_name_plural = "All Movies"
 
 
 
-	def getMovieGenres(self):
-		return self.movieGenres
+    '''
+    So, the idea behind this function is to get the data for who all gave it a "Thumbs Up".
+    This is quite a crude solution to a problem that should not exist in a good DB design, but hey, it works
+    
+    Future improvement: right now, it will query for the same data over and over. We should have it run once and store it in the DB.
+    
+    
+    returns: dictionary with total likes and the int "1" which signifies that the person gave it a thumbs up
+    
+    IMP: the critera for comparison should be that "None" will be returned if the person gave it a thumbs down  
+    '''
+    def getOverallRating(self):
+
+        totalLikes = 0
+        like_ankur = None
+        like_dipen = None
+        like_ashesh = None
+        like_shantnu = None
+        like_jayant = None
+
+        primKey = self.id
+
+
+        try:
+            movie_post = AllMovies.objects.get(id=primKey)
+        except:
+            movie_post = None
+
+
+        try:
+            ankur_post = Ankur.objects.get(post=primKey)
+            if ankur_post.verdict == 'thumbs_up':
+                totalLikes+=1
+                like_ankur = 1
+        except:
+            ankur_post = None
+
+
+        try:
+            dipen_post = Dipen.objects.get(post=primKey)
+            if dipen_post.verdict == 'thumbs_up':
+                totalLikes+=1
+                like_dipen = 1
+        except:
+            dipen_post = None
+
+
+        try:
+            shantnu_post = Shantnu.objects.get(post=primKey)
+            if shantnu_post.verdict == 'thumbs_up':
+                totalLikes+=1
+                like_shantnu = 1
+        except:
+            shantnu_post = None
+
+
+        try:
+            ashesh_post = Ashesh.objects.get(post=primKey)
+            if ashesh_post.verdict == 'thumbs_up':
+                totalLikes+=1
+                like_ashesh = 1
+        except:
+            ashesh_post = None
+
+
+        try:
+            jayant_post = Jayant.objects.get(post=primKey)
+            if jayant_post.verdict == 'thumbs_up':
+                totalLikes+=1
+                like_jayant = 1
+        except:
+            jayant_post = None
+
+
+        finally:
+
+            verdictDict = {
+                'totalLikes': totalLikes,
+                'like_ankur' : like_ankur,
+                'like_dipen' : like_dipen,
+                'like_ashesh' : like_ashesh,
+                'like_shantnu' : like_shantnu,
+                'like_jayant' : like_jayant
+            }
+
+        return verdictDict
+
+
+
+    def getMovieMoods(self):
+        return self.movieMoods
+
+
+
+    def getMovieGenres(self):
+        return self.movieGenres
 
 
 
@@ -264,22 +249,22 @@ This is Dipen's "table", which will store reviews written by Dipen
 '''
 class Dipen(models.Model):
 
-	final_verdict = (
-		('thumbs_up','Thumbs Up'),
-		('thumbs_down','Thumbs Down'),
-	)
+    final_verdict = (
+        ('thumbs_up','Thumbs Up'),
+        ('thumbs_down','Thumbs Down'),
+    )
 
-	post = models.OneToOneField(AllMovies, on_delete=models.CASCADE)
-	reviewTitle = models.CharField(max_length=300)
-	reviewBody = models.TextField()
-	reviewDate = models.DateTimeField()
-	verdict = models.CharField(max_length=11, choices=final_verdict, default='thumbs_up')
+    post = models.OneToOneField(AllMovies, on_delete=models.CASCADE)
+    reviewTitle = models.CharField(max_length=300)
+    reviewBody = models.TextField()
+    reviewDate = models.DateTimeField()
+    verdict = models.CharField(max_length=11, choices=final_verdict, default='thumbs_up')
 
-	def __str__(self):
-		return self.reviewTitle
+    def __str__(self):
+        return self.reviewTitle
 
-	class Meta:
-		verbose_name_plural = "Dipen's Reviews"
+    class Meta:
+        verbose_name_plural = "Dipen's Reviews"
 
 
 
@@ -290,22 +275,22 @@ This is Ankur's "table", which will store reviews written by Ankur
 '''
 class Ankur(models.Model):
 
-	final_verdict = (
-		('thumbs_up','Thumbs Up'),
-		('thumbs_down','Thumbs Down'),
-	)
+    final_verdict = (
+        ('thumbs_up','Thumbs Up'),
+        ('thumbs_down','Thumbs Down'),
+    )
 
-	post = models.OneToOneField(AllMovies, on_delete=models.CASCADE)
-	reviewTitle = models.CharField(max_length=300)
-	reviewBody = models.TextField()
-	reviewDate = models.DateTimeField()
-	verdict = models.CharField(max_length=11, choices=final_verdict, default='thumbs_up')
+    post = models.OneToOneField(AllMovies, on_delete=models.CASCADE)
+    reviewTitle = models.CharField(max_length=300)
+    reviewBody = models.TextField()
+    reviewDate = models.DateTimeField()
+    verdict = models.CharField(max_length=11, choices=final_verdict, default='thumbs_up')
 
-	def __str__(self):
-		return self.reviewTitle
+    def __str__(self):
+        return self.reviewTitle
 
-	class Meta:
-		verbose_name_plural = "Ankur's Reviews"
+    class Meta:
+        verbose_name_plural = "Ankur's Reviews"
 
 
 
@@ -316,22 +301,22 @@ This is Shantnu's "table", which will store reviews written by Shantnu
 '''
 class Shantnu(models.Model):
 
-	final_verdict = (
-		('thumbs_up','Thumbs Up'),
-		('thumbs_down','Thumbs Down'),
-	)
+    final_verdict = (
+        ('thumbs_up','Thumbs Up'),
+        ('thumbs_down','Thumbs Down'),
+    )
 
-	post = models.OneToOneField(AllMovies, on_delete=models.CASCADE)
-	reviewTitle = models.CharField(max_length=300)
-	reviewBody = models.TextField()
-	reviewDate = models.DateTimeField()
-	verdict = models.CharField(max_length=11, choices=final_verdict, default='thumbs_up')
+    post = models.OneToOneField(AllMovies, on_delete=models.CASCADE)
+    reviewTitle = models.CharField(max_length=300)
+    reviewBody = models.TextField()
+    reviewDate = models.DateTimeField()
+    verdict = models.CharField(max_length=11, choices=final_verdict, default='thumbs_up')
 
-	def __str__(self):
-		return self.reviewTitle
+    def __str__(self):
+        return self.reviewTitle
 
-	class Meta:
-		verbose_name_plural = "Shantnu's Reviews"
+    class Meta:
+        verbose_name_plural = "Shantnu's Reviews"
 
 
 
@@ -342,22 +327,22 @@ This is Ashesh's "table", which will store reviews written by Ashesh
 '''
 class Ashesh(models.Model):
 
-	final_verdict = (
-		('thumbs_up','Thumbs Up'),
-		('thumbs_down','Thumbs Down'),
-	)
+    final_verdict = (
+        ('thumbs_up','Thumbs Up'),
+        ('thumbs_down','Thumbs Down'),
+    )
 
-	post = models.OneToOneField(AllMovies, on_delete=models.CASCADE)
-	reviewTitle = models.CharField(max_length=300)
-	reviewBody = models.TextField()
-	reviewDate = models.DateTimeField()
-	verdict = models.CharField(max_length=11, choices=final_verdict, default='thumbs_up')
+    post = models.OneToOneField(AllMovies, on_delete=models.CASCADE)
+    reviewTitle = models.CharField(max_length=300)
+    reviewBody = models.TextField()
+    reviewDate = models.DateTimeField()
+    verdict = models.CharField(max_length=11, choices=final_verdict, default='thumbs_up')
 
-	def __str__(self):
-		return self.reviewTitle
+    def __str__(self):
+        return self.reviewTitle
 
-	class Meta:
-		verbose_name_plural = "Ashesh's Reviews"
+    class Meta:
+        verbose_name_plural = "Ashesh's Reviews"
 
 
 
@@ -368,19 +353,19 @@ This is Jayant's "table", which will store reviews written by Jayant
 '''
 class Jayant(models.Model):
 
-	final_verdict = (
-		('thumbs_up','Thumbs Up'),
-		('thumbs_down','Thumbs Down'),
-	)
+    final_verdict = (
+        ('thumbs_up','Thumbs Up'),
+        ('thumbs_down','Thumbs Down'),
+    )
 
-	post = models.OneToOneField(AllMovies, on_delete=models.CASCADE)
-	reviewTitle = models.CharField(max_length=300)
-	reviewBody = models.TextField()
-	reviewDate = models.DateTimeField()
-	verdict = models.CharField(max_length=11, choices=final_verdict, default='thumbs_up')
+    post = models.OneToOneField(AllMovies, on_delete=models.CASCADE)
+    reviewTitle = models.CharField(max_length=300)
+    reviewBody = models.TextField()
+    reviewDate = models.DateTimeField()
+    verdict = models.CharField(max_length=11, choices=final_verdict, default='thumbs_up')
 
-	def __str__(self):
-		return self.reviewTitle
+    def __str__(self):
+        return self.reviewTitle
 
-	class Meta:
-		verbose_name_plural = "Jayant's Reviews"
+    class Meta:
+        verbose_name_plural = "Jayant's Reviews"
