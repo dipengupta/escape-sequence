4570e3df571de1162b9e774b2b057a753b9280c1
dipeng <dipeng@urlinsgroup.com>
2025-03-30 11:36:32 +0530

    database pyanywhere change

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,22 +75,22 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'dipengupta$prod',
-#         'USER': 'dipengupta',
-#         'PASSWORD': 'makeSiteFaster',
-#         'HOST': 'dipengupta.mysql.pythonanywhere-services.com',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dipengupta$prod',
+        'USER': 'dipengupta',
+        'PASSWORD': 'makeSiteFaster',
+        'HOST': 'dipengupta.mysql.pythonanywhere-services.com',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 
